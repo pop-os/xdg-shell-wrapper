f93d80afa9265c055640f596374fcfb6cc8786f1
Ashley Wulber <ashley@system76.com>
2022-08-03 21:32:57 -0400

    feat: kbd event handling

--- src/client/handlers/keyboard.rs
+++ src/client/handlers/keyboard.rs
@@ -1,181 +1,189 @@
-use crate::{space::WrapperSpace, shared_state::GlobalState};
-use sctk::{seat::keyboard::KeyboardHandler, delegate_keyboard};
+use std::time::Instant;
 
-
-// pub fn send_keyboard_event<W: WrapperSpace + 'static>(
-//     event: wl_keyboard::Event,
-//     seat_name: &str,
-//     mut dispatch_data: DispatchData<'_>,
-// ) {
-//     let (state, server_display) = dispatch_data
-//         .get::<(GlobalState<W>, Display<GlobalState<W>>)>()
-//         .unwrap();
-//     let dh = server_display.handle();
-//     let space = &mut state.space;
-//     let ClientState {
-//         last_input_serial,
-//         focused_surface: c_focused_surface,
-//         ..
-//     } = &mut state.client_state;
-//     let ServerState { seats, .. } = &mut state.server_state;
-//     if let Some(seat) = seats.iter().find(|SeatPair { name, .. }| name == seat_name) {
-//         let kbd = match seat.server.get_keyboard() {
-//             Some(kbd) => kbd,
-//             None => {
-//                 error!(
-//                     state.log,
-//                     "Received keyboard event on {} without keyboard.", &seat_name
-//                 );
-//                 return;
-//             }
-//         };
-//         match event {
-//             wl_keyboard::Event::Key {
-//                 serial,
-//                 time,
-//                 key,
-//                 state,
-//             } => {
-//                 last_input_serial.replace(serial);
-//                 let state = match state {
-//                     client::protocol::wl_keyboard::KeyState::Pressed => KeyState::Pressed,
-//                     client::protocol::wl_keyboard::KeyState::Released => KeyState::Released,
-//                     _ => return,
-//                 };
-//                 match kbd.input::<(), _>(
-//                     &dh,
-//                     key,
-//                     state,
-//                     SERIAL_COUNTER.next_serial(),
-//                     time,
-//                     move |_modifiers, keysym| {
-//                         if keysym.modified_sym() == XKB_KEY_Escape && state == KeyState::Released {
-//                             FilterResult::Intercept(())
-//                         } else {
-//                             FilterResult::Forward
-//                         }
-//                     },
-//                 ) {
-//                     Some(_) => {
-//                         space.keyboard_leave(seat_name, None);
-//                         kbd.set_focus(&dh, None, SERIAL_COUNTER.next_serial());
-//                     }
-//                     None => {}
-//                 }
-//             }
-//             wl_keyboard::Event::RepeatInfo { rate, delay } => {
-//                 kbd.change_repeat_info(rate, delay);
-//             }
-//             wl_keyboard::Event::Enter { surface, .. } => {
-//                 // TODO data device
-//                 // let _ = set_data_device_selection(
-//                 //     dh,
-//                 //     &seat.client.seat,
-//                 //     &seat.server,
-//                 //     &selected_data_provider.seat,
-//                 // );
-//                 // let _client = focused_surface
-//                 //     .borrow()
-//                 //     .as_ref()
-//                 //     .and_then(|focused_surface| focused_surface.client_id());
-
-//                 // TODO data device
-//                 // set_data_device_focus(&seat.server.0, client);
-
-//                 {
-//                     let mut c_focused_surface = c_focused_surface.borrow_mut();
-//                     if let Some(i) = c_focused_surface.iter().position(|f| f.1 == seat_name) {
-//                         c_focused_surface[i].0 = surface.clone();
-//                         c_focused_surface[i].2 = FocusStatus::Focused;
-//                     } else {
-//                         c_focused_surface.push((
-//                             surface.clone(),
-//                             seat_name.to_string(),
-//                             FocusStatus::Focused,
-//                         ));
-//                     }
-//                 }
-
-//                 let s = space.keyboard_enter(seat_name, surface);
-
-//                 kbd.set_focus(&dh, s.as_ref(), SERIAL_COUNTER.next_serial());
-//             }
-//             wl_keyboard::Event::Leave { surface, .. } => {
-//                 let kbd_focus = {
-//                     let mut c_focused_surface = c_focused_surface.borrow_mut();
-//                     if let Some(i) = c_focused_surface.iter().position(|f| f.0 == surface) {
-//                         c_focused_surface[i].2 = FocusStatus::LastFocused(Instant::now());
-//                         true
-//                     } else {
-//                         false
-//                     }
-//                 };
-//                 if kbd_focus {
-//                     space.keyboard_leave(seat_name, Some(surface));
-//                     kbd.set_focus(&dh, None, SERIAL_COUNTER.next_serial());
-//                 }
-//             }
-//             _ => (),
-//         };
-//     }
-// }
+use crate::{space::WrapperSpace, shared_state::GlobalState, server_state::SeatPair, client_state::FocusStatus};
+use sctk::{seat::keyboard::{KeyboardHandler, keysyms::XKB_KEY_Escape, RepeatInfo}, delegate_keyboard};
+use smithay::{wayland::{SERIAL_COUNTER, seat::FilterResult}, backend::input::KeyState};
 
 impl<W: WrapperSpace> KeyboardHandler for GlobalState<W> {
     fn enter(
         &mut self,
-        conn: &sctk::reexports::client::Connection,
-        qh: &sctk::reexports::client::QueueHandle<Self>,
+        _conn: &sctk::reexports::client::Connection,
+        _qh: &sctk::reexports::client::QueueHandle<Self>,
         keyboard: &sctk::reexports::client::protocol::wl_keyboard::WlKeyboard,
         surface: &sctk::reexports::client::protocol::wl_surface::WlSurface,
-        serial: u32,
-        raw: &[u32],
-        keysyms: &[u32],
+        _serial: u32,
+        _raw: &[u32],
+        _keysyms: &[u32],
     ) {
-        todo!()
+        let (seat_name, kbd) = 
+            if let Some((name, Some(kbd))) = self.server_state.seats
+                .iter()
+                .find(|SeatPair { client, .. }| {
+                    client.kbd.as_ref().map(|k| k == keyboard).unwrap_or(false)
+                })
+                .map(|seat| (seat.name.as_str(), seat.server.get_keyboard()))
+            {
+                (name.to_string(), kbd).clone()
+            } else {
+                return ;
+            };    
+        
+        {
+            let mut c_focused_surface = self.client_state.focused_surface.borrow_mut();
+            if let Some(i) = c_focused_surface.iter().position(|f| f.1 == seat_name) {
+                c_focused_surface[i].0 = surface.clone();
+                c_focused_surface[i].2 = FocusStatus::Focused;
+            } else {
+                c_focused_surface.push((
+                    surface.clone(),
+                    seat_name.to_string(),
+                    FocusStatus::Focused,
+                ));
+            }
+        }
+
+        let s = self.space.keyboard_enter(&seat_name, surface.clone());
+        kbd.set_focus(&self.server_state.display_handle, s.as_ref(), SERIAL_COUNTER.next_serial());
     }
 
     fn leave(
         &mut self,
-        conn: &sctk::reexports::client::Connection,
-        qh: &sctk::reexports::client::QueueHandle<Self>,
+        _conn: &sctk::reexports::client::Connection,
+        _qh: &sctk::reexports::client::QueueHandle<Self>,
         keyboard: &sctk::reexports::client::protocol::wl_keyboard::WlKeyboard,
         surface: &sctk::reexports::client::protocol::wl_surface::WlSurface,
-        serial: u32,
+        _serial: u32,
     ) {
-        todo!()
+        let (seat_name, kbd) = 
+            if let Some((name, Some(kbd))) = self.server_state.seats
+                .iter()
+                .find(|SeatPair { client, .. }| {
+                    client.kbd.as_ref().map(|k| k == keyboard).unwrap_or(false)
+                })
+                .map(|seat| (seat.name.as_str(), seat.server.get_keyboard()))
+            {
+                (name.to_string(), kbd).clone()
+            } else {
+                return ;
+            };   
+
+        let kbd_focus = {
+            let mut c_focused_surface = self.client_state.focused_surface.borrow_mut();
+            if let Some(i) = c_focused_surface.iter().position(|f| &f.0 == surface) {
+                c_focused_surface[i].2 = FocusStatus::LastFocused(Instant::now());
+                true
+            } else {
+                false
+            }
+        };
+        if kbd_focus {
+            self.space.keyboard_leave(&seat_name, Some(surface.clone()));
+            kbd.set_focus(&self.server_state.display_handle, None, SERIAL_COUNTER.next_serial());
+        }
     }
 
     fn press_key(
         &mut self,
-        conn: &sctk::reexports::client::Connection,
-        qh: &sctk::reexports::client::QueueHandle<Self>,
+        _conn: &sctk::reexports::client::Connection,
+        _qh: &sctk::reexports::client::QueueHandle<Self>,
         keyboard: &sctk::reexports::client::protocol::wl_keyboard::WlKeyboard,
-        serial: u32,
+        _serial: u32,
         event: sctk::seat::keyboard::KeyEvent,
     ) {
-        todo!()
+        let kbd = 
+            if let Some(Some(kbd)) = self.server_state.seats
+                .iter()
+                .find_map(|SeatPair { client, server, .. }| {
+                    client.kbd.as_ref().map(|k| if k == keyboard {server.get_keyboard()} else {None})
+                })
+            {
+                kbd.clone()
+            } else {
+                return ;
+            };   
+
+        let _ = kbd.input::<(), _>(
+            &self.server_state.display_handle,
+            event.keysym,
+            KeyState::Pressed,
+            SERIAL_COUNTER.next_serial(),
+            event.time,
+            move |_modifiers, _keysym| FilterResult::Forward
+        );   
     }
 
     fn release_key(
         &mut self,
-        conn: &sctk::reexports::client::Connection,
-        qh: &sctk::reexports::client::QueueHandle<Self>,
+        _conn: &sctk::reexports::client::Connection,
+        _qh: &sctk::reexports::client::QueueHandle<Self>,
         keyboard: &sctk::reexports::client::protocol::wl_keyboard::WlKeyboard,
-        serial: u32,
+        _serial: u32,
         event: sctk::seat::keyboard::KeyEvent,
     ) {
-        todo!()
+        let (seat_name, kbd) = 
+            if let Some((name, Some(kbd))) = self.server_state.seats
+                .iter()
+                .find(|SeatPair { client, .. }| {
+                    client.kbd.as_ref().map(|k| k == keyboard).unwrap_or(false)
+                })
+                .map(|seat| (seat.name.as_str(), seat.server.get_keyboard()))
+            {
+                (name.to_string(), kbd).clone()
+            } else {
+                return ;
+            };   
+
+        match kbd.input::<(), _>(
+            &self.server_state.display_handle,
+            event.keysym,
+            KeyState::Released,
+            SERIAL_COUNTER.next_serial(),
+            event.time,
+            move |_modifiers, keysym| {
+                if keysym.modified_sym() == XKB_KEY_Escape {
+                    FilterResult::Intercept(())
+                } else {
+                    FilterResult::Forward
+                }
+            },
+        ) {
+            Some(_) => {
+                self.space.keyboard_leave(&seat_name, None);
+                kbd.set_focus(&self.server_state.display_handle, None, SERIAL_COUNTER.next_serial());
+            }
+            None => {}
+        };
+    }
+
+    fn update_repeat_info(
+        &mut self,
+        _conn: &sctk::reexports::client::Connection,
+        _qh: &sctk::reexports::client::QueueHandle<Self>,
+        kbd: &sctk::reexports::client::protocol::wl_keyboard::WlKeyboard,
+        info: RepeatInfo,
+    ) {
+        if let Some(kbd) = self.server_state.seats
+            .iter()
+            .find_map(|SeatPair { client, server, .. }| {
+                client.kbd.as_ref().and_then(|k| if k == kbd {server.get_keyboard()} else {None})
+            })
+        {
+            match info {
+                RepeatInfo::Repeat { rate, delay } => kbd.change_repeat_info(u32::from(rate) as i32, delay.try_into().unwrap()),
+                RepeatInfo::Disable => {kbd.change_repeat_info(0, 0)},
+            };
+        }
     }
 
     fn update_modifiers(
         &mut self,
-        conn: &sctk::reexports::client::Connection,
-        qh: &sctk::reexports::client::QueueHandle<Self>,
-        keyboard: &sctk::reexports::client::protocol::wl_keyboard::WlKeyboard,
-        serial: u32,
-        modifiers: sctk::seat::keyboard::Modifiers,
+        _conn: &sctk::reexports::client::Connection,
+        _qh: &sctk::reexports::client::QueueHandle<Self>,
+        _keyboard: &sctk::reexports::client::protocol::wl_keyboard::WlKeyboard,
+        _serial: u32,
+        _modifiers: sctk::seat::keyboard::Modifiers,
     ) {
-        todo!()
+        // TODO should these be handled specially
     }
 }
 
