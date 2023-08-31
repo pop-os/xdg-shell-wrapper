28975d148637941d149192bc22bdd1a1ecb6d69b
Ashley Wulber <ashley.wulber@pm.me>
2023-08-31 15:44:17 -0400

    chore: update deps

--- src/client/handlers/compositor.rs
+++ src/client/handlers/compositor.rs
@@ -2,8 +2,9 @@
 
 use sctk::{
     compositor::CompositorHandler,
-    reexports::client::{protocol::wl_surface, Connection, QueueHandle},
+    reexports::client::{protocol::wl_surface, Connection, QueueHandle}, shell::WaylandSurface,
 };
+use smithay::reexports::wayland_server::protocol::wl_output::Transform;
 
 use crate::{shared_state::GlobalState, space::WrapperSpace};
 
@@ -37,5 +38,40 @@ impl<W: WrapperSpace> CompositorHandler for GlobalState<W> {
         } else {
             self.space.frame(surface, time);
         }
+    }
+
+    fn transform_changed(
+        &mut self,
+        conn: &Connection,
+        qh: &QueueHandle<Self>,
+        surface: &wl_surface::WlSurface,
+        new_transform: sctk::reexports::client::protocol::wl_output::Transform,
+    ) {
+        for tracked_surface in &mut self.client_state.proxied_layer_surfaces {
+            if tracked_surface.3.wl_surface() == surface {
+                let transform = match new_transform {
+                    sctk::reexports::client::protocol::wl_output::Transform::Normal => Transform::Normal,
+                    sctk::reexports::client::protocol::wl_output::Transform::_90 => Transform::_90,
+                    sctk::reexports::client::protocol::wl_output::Transform::_180 => Transform::_180,
+                    sctk::reexports::client::protocol::wl_output::Transform::_270 => Transform::_270,
+                    sctk::reexports::client::protocol::wl_output::Transform::Flipped => Transform::Flipped,
+                    sctk::reexports::client::protocol::wl_output::Transform::Flipped90 => Transform::Flipped90,
+                    sctk::reexports::client::protocol::wl_output::Transform::Flipped180 => Transform::Flipped180,
+                    sctk::reexports::client::protocol::wl_output::Transform::Flipped270 => Transform::Flipped270,
+                    _ => {
+                        tracing::warn!("Received unknown transform.");
+                        return;
+                    },
+                };
+                tracked_surface.2.wl_surface().preferred_buffer_transform(transform);
+                return;
+            }
+        }
+
+        self.space
+            .transform_changed(conn,
+        surface,
+        new_transform);
+
     }
 }
